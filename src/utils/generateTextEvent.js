14d48a701cb2fc7ca8325b252b6f5dfaf300bb78
Taump <taump@yandex.ru>
2022-08-16 22:25:55 +0600

    fix typo

--- src/utils/generateTextEvent.js
+++ src/utils/generateTextEvent.js
@@ -9,14 +9,14 @@ export const generateTextEvent = ({ oracle, event_date, feed_name, datafeed_valu
     if (appConfig.CATEGORIES.currency.oracles.find(({ address }) => address === oracle)) {
         const [from, to] = feed_name.split("_");
 
-        return `Will ${from} be ${comparisonText} ${datafeed_value} ${to} on ${expiry_date} ${isUTC ? 'UTC' : ''}?`;
+        return `Will ${from} be ${comparisonText} ${datafeed_value} ${to} on ${expiry_date}${isUTC ? ' UTC' : ''}?`;
     } else if (appConfig.CATEGORIES.sport.oracles.find(({ address }) => address === oracle)) {
         // eslint-disable-next-line no-unused-vars
         const [_, yes_team, no_team] = feed_name.split("_");
 
         return `${yes_team} vs ${no_team} for ${expiry_date}`;
     } else {
-        return `Will ${feed_name} be ${comparisonText} ${datafeed_value} on ${expiry_date} ${isUTC ? 'UTC' : ''}?`;
+        return `Will ${feed_name} be ${comparisonText} ${datafeed_value} on ${expiry_date}${isUTC ? ' UTC' : ''}?`;
     }
 }
 
