84925b63fb8bdb57948053fc38c8285de53e6831
Alexander Ponomarev <taump@yandex.ru>
2023-01-09 18:22:26 +0100

    fix: odds for completed markets

--- src/pages/MarketPage/MarketPage.js
+++ src/pages/MarketPage/MarketPage.js
@@ -332,11 +332,10 @@ export const MarketPage = () => {
 
   if (result && reserve) {
     const winnerSupply = result === 'yes' ? supply_yes : (result === 'no' ? supply_no : supply_draw);
-    const winnerPrice = result === 'yes' ? yesPrice : (result === 'no' ? noPrice : drawPrice);
 
     if (winnerSupply) {
       winnerPriceView = +Number(reserve / winnerSupply).toPrecision(5);
-      winnerOddsView = +Number(reserve / (winnerPrice * winnerSupply)).toPrecision(5);
+      winnerOddsView = +Number(reserve ** 2 / (winnerSupply ** 2 * coef ** 2)).toPrecision(5);
     }
   }
 
