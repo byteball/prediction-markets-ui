f889795932205962c8992da0ff0b6dc1e087f472
Taump <taump@yandex.ru>
2024-05-27 16:01:05 +0400

    fix: language selection logic

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 import moment from "moment";
 
 import 'moment/locale/es';
-import 'moment/locale/pt';
+import 'moment/locale/pt-br';
 import 'moment/locale/zh-cn';
 import 'moment/locale/ru';
 import 'moment/locale/uk';
@@ -14,29 +14,58 @@ import { CreatePage, MainPage, MarketPage, FaqPage } from "pages";
 import { historyInstance } from "historyInstance";
 import { changeLanguage, selectLanguage } from "store/slices/settingsSlice";
 
+import { botCheck } from "utils";
+
 import { langs } from "components/SelectLanguage/SelectLanguage";
 import { Layout } from "components/Layout/Layout";
 
 import i18 from './locale/index';
+
+const DEFAULT_LANGUAGE_KEY = 'en';
 
 const AppRouter = () => {
   const lang = useSelector(selectLanguage);
   const dispatch = useDispatch()
 
   useEffect(() => {
-    if (!lang) {
-      const firstPath = window.location.pathname.split("/")?.[1];
+    const pathname = window.location.pathname;
+    const langList = langs.map((lang) => lang.name);
+    const languageInUrl = langList.includes(pathname.split("/")[1]) ? pathname.split("/")[1] : null;
+    let cleanedUrl = pathname;
 
-      if (firstPath && langs.find((lang) => lang.name === firstPath)) {
-        dispatch(changeLanguage(firstPath));
-        moment.locale(firstPath === 'zh' ? 'zh-cn' : firstPath);
-      } else {
-        dispatch(changeLanguage("en"));
-        moment.locale("en");
+    if (languageInUrl) {
+      cleanedUrl = pathname.replace('/' + languageInUrl, "");
+    }
+
+    if (!lang) {
+      const languageFromBrowserSettings = navigator.language.split("-")[0];
+
+      const language = botCheck() ? languageInUrl : (languageInUrl || languageFromBrowserSettings);
+
+      if (language && langList.find((lang) => lang === language)) { // if language is in the list
+        dispatch(changeLanguage(language));
+        moment.locale(getMomentLocaleByLanguageKey(language));
+
+        if (language !== languageInUrl) {
+          historyInstance.replace(`${language !== "en" ? '/' + language : ""}${cleanedUrl === "/" && language !== "en" ? "" : cleanedUrl}`);
+        }
+
+      } else { // if language is not in the list we use default language
+        i18.changeLanguage(DEFAULT_LANGUAGE_KEY);
+        historyInstance.replace(cleanedUrl);
+        moment.locale(getMomentLocaleByLanguageKey(DEFAULT_LANGUAGE_KEY));
       }
-    } else {
+
+    } else { // if language is already set
       i18.changeLanguage(lang);
-      moment.locale(lang === 'zh' ? 'zh-cn' : lang);
+
+      if (lang !== languageInUrl) {
+        historyInstance.replace(`${lang !== "en" ? '/' + lang : ""}${cleanedUrl === "/" ? "" : cleanedUrl}`);
+      } else if (lang === "en" && languageInUrl === "en") {
+        historyInstance.replace(cleanedUrl);
+      }
+
+      moment.locale(getMomentLocaleByLanguageKey(lang));
     }
 
   }, [lang]);
@@ -63,3 +92,10 @@ const AppRouter = () => {
 }
 
 export default AppRouter;
+
+const getMomentLocaleByLanguageKey = (languageKey) => {
+  if (languageKey === 'zh') return 'zh-cn';
+  if (languageKey === 'pt') return 'pt-br';
+
+  return languageKey;
+}
