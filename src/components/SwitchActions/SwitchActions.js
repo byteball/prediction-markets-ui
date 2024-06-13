d747e93ccb92dbaaa2a5fca99bc8049b32e14ab0
Taump <taump@yandex.ru>
2024-06-13 21:28:16 +0400

    feat: linked switch

--- src/components/SwitchActions/SwitchActions.js
+++ src/components/SwitchActions/SwitchActions.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 import styles from "./SwitchActions.module.css";
 
-export const SwitchActions = ({ data = [], value, onChange, small = false }) => {
+export const SwitchActions = ({ data = [], value, onChange, small = false, linked = false }) => {
   const [currentValue, setCurrentValue] = useState(value || data[0]?.value);
 
   useEffect(() => {
@@ -18,8 +19,16 @@ export const SwitchActions = ({ data = [], value, onChange, small = false }) => 
   }, [currentValue]);
 
   if (data.length === 0) return null;
+  
+  const handleSwitch = (e, value) => {
+    e.preventDefault();
+
+    setCurrentValue(value);
+  }
+
+  const Wrapper = linked ? Link : 'span';
 
   return <div className={`${styles.switch} ${small ? styles.switchSmall : ''}`}>
-    {data.map(({ text, value, iconLink }) => <span key={value} onClick={() => setCurrentValue(value)} className={`${styles.switchItem} ${currentValue === value ? styles.switchActive : ""}`}>{iconLink && <img src={iconLink} style={{ background: "#fff", height: '1em' }} alt={text} />}{text}</span>)}
+    {data.map(({ text, value, iconLink, url }) => <Wrapper to={linked ? url : undefined} key={value} onClick={(e) => handleSwitch(e, value)} className={`${styles.switchItem} ${currentValue === value ? styles.switchActive : ""}`}>{iconLink && <img src={iconLink} style={{ background: "#fff", height: '1em' }} alt={text} />}{text}</Wrapper>)}
   </div>
 }
