3267b2ef565927446837cba53dd8191cfa0992ba
Alexander Ponomarev <taump@yandex.ru>
2026-06-25 16:54:27 +0200

    feat: add winner display with crest and styling

--- src/modals/ClaimProfitModal.js
+++ src/modals/ClaimProfitModal.js
@@ -3,9 +3,11 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Helmet } from "react-helmet-async";
 import { useTranslation } from "react-i18next";
+import { Img } from "react-image";
 
 import { ClaimProfitForm } from "forms";
 import { useWindowSize } from "hooks";
+import styles from "./ClaimProfitModal.module.css";
 import {
   selectActiveAddress,
   selectActiveMarketParams,
@@ -23,7 +25,7 @@ export const ClaimProfitModal = ({ disabled, yes_team, no_team }) => {
   const stateVars = useSelector(selectActiveMarketStateVars);
   const walletAddress = useSelector(selectWalletAddress);
 
-  const { yes_decimals, no_decimals, draw_decimals, yes_symbol, no_symbol, draw_symbol, reserve_decimals, reserve_symbol } = useSelector(selectActiveMarketParams);
+  const { yes_decimals, no_decimals, draw_decimals, yes_symbol, no_symbol, draw_symbol, reserve_decimals, reserve_symbol, yes_crest_url, no_crest_url } = useSelector(selectActiveMarketParams);
 
   const { result: winner, supply_yes, supply_no, supply_draw, reserve, yes_asset, no_asset, draw_asset } = stateVars;
 
@@ -32,6 +34,7 @@ export const ClaimProfitModal = ({ disabled, yes_team, no_team }) => {
   const asset = (winner === 'yes' ? yes_asset : (winner === 'no' ? no_asset : draw_asset) || 0);
   const symbol = (winner === 'yes' ? yes_symbol : (winner === 'no' ? no_symbol : draw_symbol) || 0);
   const winnerView = winner === 'yes' ? yes_team || 'Yes' : (winner === 'no' ? no_team || 'No' : 'Draw');
+  const winnerCrest = winner === 'yes' ? yes_crest_url : (winner === 'no' ? no_crest_url : null);
 
   const [width] = useWindowSize();
   const { t } = useTranslation();
@@ -51,7 +54,19 @@ export const ClaimProfitModal = ({ disabled, yes_team, no_team }) => {
     >
       <Title level={2}>{t("modals.claim_profit.title", "Claim profit")}</Title>
 
-      <Typography.Paragraph type="secondary">
+      <div className={styles.banner}>
+        {winnerCrest && <div className={styles.flagWrap}>
+          <Img src={winnerCrest} alt={winnerView} className={styles.flag} />
+        </div>}
+        <div className={styles.nameRow}>
+          <img className={styles.wreath} src="/laurel-wreath.svg" alt="" aria-hidden="true" />
+          <span className={`${styles.winnerName} ${winnerCrest ? '' : styles.winnerNameLarge}`}>{winnerView}</span>
+          <img className={`${styles.wreath} ${styles.wreathRight}`} src="/laurel-wreath.svg" alt="" aria-hidden="true" />
+        </div>
+        <div className={styles.winnerLabel}>{t("common.winner", "WINNER")}</div>
+      </div>
+
+      <Typography.Paragraph className={styles.desc}>
         {t("modals.claim_profit.winner_desc", '"{{winner}}" was the right choice and you can collect your winnings', { winner: winnerView })}
       </Typography.Paragraph>
 
